ce7622f9bb32ea91d13dc333c4b5c19e6cc53957
Michael Gafah <michaelgafah@gmail.com>
2025-06-09 13:35:37 +0000

    Update text in CheckoutPage and Footer components for clarity and consistency

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,10 +24,10 @@ export default function Footer() {
           </FooterNav>
         </FooterTopRow>
         <FooterDescription>
-          Audiophile is an all in one stop to fulfill your audio needs. We're a
-          small team of music lovers and sound specialists who are devoted to
-          helping you get the most out of personal audio. Come and visit our
-          demo facility - we're open 7 days a week.
+          Audiophile is an all in one stop to fulfill your audio needs.
+          We&apos;re a small team of music lovers and sound specialists who are
+          devoted to helping you get the most out of personal audio. Come and
+          visit our demo facility - we&apos;re open 7 days a week.
         </FooterDescription>
         <FooterBottomRow>
           <Copyright>Copyright 2021. All Rights Reserved</Copyright>
